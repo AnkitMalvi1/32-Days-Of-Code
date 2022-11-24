525c54ab4731e19f06f5bfe2424aee0d16c1e047
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-24 13:20:12 +0530

    Add files via upload

---
+++ 1. Programming Language & Fundamentals [1-10]/1.1 C Programming/C4. Loop Control Statements/1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    // for loop
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // While loop
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+
+    // do while loop
+    int i = 1;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
