1ce97c30b1087e4646f1cc27c552f805c04b63ce
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-24 15:15:57 +0530

    Add files via upload

---
+++ 1. Programming Language & Fundamentals [1-10]/1.1 C Programming/C7. Arrays/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks[3];
+    printf("physics: ");
+    scanf("%d", &marks[0]);
+
+    printf("chemistry: ");
+    scanf("%d", &marks[1]);
+
+    printf("maths: ");
+    scanf("%d", &marks[2]);
+
+    printf("physics=%d\n", marks[0]);
+    printf("chemistry=%d\n", marks[1]);
+    printf("maths=%d\n", marks[2]);
+
+    return 0;
+}
