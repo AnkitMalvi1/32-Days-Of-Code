df584df31200e79d91b1ed589626a30c77600b1f
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-26 23:01:06 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D006/03. Data Types & Type Modifiers/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;  // declaration
+    a = 12; // initialization
+    cout << sizeof(a) << endl;
+    float b;
+    cout << sizeof(b) << endl;
+    char c;
+    cout << sizeof(c) << endl;
+    bool d;
+    cout << sizeof(d) << endl;
+    short int si;
+    cout << sizeof(si) << endl;
+    long int li;
+    cout << sizeof(li);
+
+    return 0;
+}
