4133c476c85b20d2a2254efbbb2c7e4fa57ec750
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-27 12:09:44 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D007/2. If Else Statement/3.cpp
@@ -0,0 +1,21 @@
+// Check if even or odd
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << "even";
+    }
+    else
+    {
+        cout << "Odd";
+    }
+
+    return 0;
+}
