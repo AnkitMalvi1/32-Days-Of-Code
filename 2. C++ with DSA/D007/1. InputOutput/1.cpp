00a311d4cc3f5fe51d67880cae48a4ee22c52e69
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-27 11:12:00 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D007/1. InputOutput/1.cpp
@@ -0,0 +1,20 @@
+// Cpp Program Described.
+
+#include <iostream>
+using namespace std;
+
+int main() // the execution of code begins from main function (mandatory for any program)
+{
+    std::cout << "Hello world\n"; // used to display output in quotation marks
+
+    return 0; // exit status of a function
+}
+
+// #include  -> preprocessor directive used to include files
+// iostream  -> header file for taking input & printing output
+// ;      -> marks the end of a statement
+// {  }   -> curly braces indicate the start & end of a function
+// \n   -> adds line break
+// std  -> standard -> using namespace std; // no more use of std
+// >>  -> extraction oprator (input)
+// <<  -> insertion oprator (output)
