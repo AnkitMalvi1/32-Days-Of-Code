8a2fe27bc763af8e6411bd9848b6892c8690b617
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-11-29 17:58:43 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D009/5. Functions Challenges/1.cpp
@@ -0,0 +1,33 @@
+// 1. Print all Print no.s using function
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num)
+{
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i;
+        }
+    }
+
+    return 0;
+}
