9ce50a87980a343c2f93c0981d631f43835d2189
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-12-06 16:30:32 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D016/3. Character Arrays/1.cpp
@@ -0,0 +1,17 @@
+// 1. Print character array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[100] = "apple";
+    int i = 0;
+
+    while (arr[i] != '\0')
+    {
+        cout << arr[i] << endl;
+        i++;
+    }
+
+    return 0;
+}
---
+++ 2. C++ with DSA/D016/3. Character Arrays/2.cpp
@@ -0,0 +1,14 @@
+// 2. Input & Output in char array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    cin >> arr;
+
+    cout << arr;
+    cout << arr[2];
+
+    return 0;
+}
