4145211941ef9e80da80c4d08dd9419da795b094
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2022-12-10 18:57:48 +0530

    Add files via upload

---
+++ 2. C++ with DSA/D020/1. Recursion - Introduction/1.cpp
@@ -0,0 +1,23 @@
+// 1. Recursive Sum :- /Add all numbers till n
+
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int prevSum = sum(n - 1);
+    return n + prevSum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sum(n) << endl;
+
+    return 0;
+}
